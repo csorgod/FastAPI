a560406f6420047f9a0d89969f5d641269b922a4
Guilherme Csorgo Henriques <csorgodev@hotmail.com>
2022-05-31 18:25:31 -0300

    Alterado algumas coisas no recurso cat

--- src/endpoints/cat.py
+++ src/endpoints/cat.py
@@ -10,17 +10,22 @@ from src.dependencies import database
 router = APIRouter(
     prefix="/cats",
     tags=["Cats"],
-    responses={ 404: { "description": "Not found" } },
+    responses={
+        404: {"message": "Not found"}
+    },
 )
 
-@router.get("", response_model = List[CatResponse], response_model_exclude_unset=True, status_code = status.HTTP_200_OK)
-async def get_all(skip = 0, limit = 10, db: Session = Depends(database.get_db)):
-    return CatService.get_all(db, skip = skip, limit = limit)
 
-@router.get("/{id}", response_model = CatResponse, response_model_exclude_unset=True, status_code = status.HTTP_200_OK)
+@router.get("", response_model=List[CatResponse], response_model_exclude_unset=True, status_code=status.HTTP_200_OK)
+async def get_all(skip=0, limit=10, db: Session = Depends(database.get_db)):
+    return CatService.get_all(db, skip=skip, limit=limit)
+
+
+@router.get("/{id}", response_model=CatResponse, response_model_exclude_unset=True, status_code=status.HTTP_200_OK)
 async def get_by_id(id: int, db: Session = Depends(database.get_db)):
-   return CatService.get_by_id(id)
+    return CatService.get_by_id(db, id)
 
-@router.post("", status_code = status.HTTP_201_CREATED)
+
+@router.post("", status_code=status.HTTP_201_CREATED)
 async def get_by_id(cat: Cat, db: Session = Depends(database.get_db)):
     return CatService.create_new(db, cat)
